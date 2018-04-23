e140846d3ac5f8cfac3ad56a0b94e1f0a80abe25
Michael Eby <michaeleby1@gmail.com>
2018-04-23 00:34:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,9 +3,4 @@ var recipes = { 'Pasta': 'Sauce'}
 function updateObjectWithKeyAndValue(recipes, dish, ingredients) {
 recipes[dish] = ingredients
 return recipes
-
 }
-
-
-  1) Objects updateObjectWithKeyAndValue(object, key, value) returns an object with the orignal key value pairs and
-   the new key value pair:
