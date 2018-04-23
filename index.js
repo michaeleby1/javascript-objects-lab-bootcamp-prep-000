e79c722acf01dd9e552d280f693a17fe08c04fee
Michael Eby <michaeleby1@gmail.com>
2018-04-23 01:26:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function destructivelyDeleteFromObjectByKey(recipes, dish) {
 };
 
 function destructivelyDeleteFromObjectByKey(recipes, dish) {
-  var recipes = Object.assign({}, {recipes})
+
   delete recipes.dish
   return recipes
 }
