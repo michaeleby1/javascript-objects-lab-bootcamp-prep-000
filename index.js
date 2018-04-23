4bce0f44779c0cf28649573724ac9c52d7c5e02b
Michael Eby <michaeleby1@gmail.com>
2018-04-23 00:52:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,12 @@ function updateObjectWithKeyAndValue(recipes, dish, ingredients) {
 function updateObjectWithKeyAndValue(recipes, dish, ingredients) {
   return Object.assign({ prop: 1 }, { prop2: 2 })
 };
+
+function destructivelyUpdateObjectWithKeyAndValue(recipes, dish, ingredients) {
+  recipes[dish]: ingredients
+  return recipes
+}
+
+1) Objects destructivelyUpdateObjectWithKeyAndValue(object, key, value)
+updates `object` with the given `key` and `value` (it is destructive) and re
+turns the entire updated object:
