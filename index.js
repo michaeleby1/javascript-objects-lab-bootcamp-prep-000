f60c86629dbc57515a9c8b3d336fae9c19183cd3
Michael Eby <michaeleby1@gmail.com>
2018-04-23 00:53:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,3 @@ function destructivelyUpdateObjectWithKeyAndValue(recipes, dish, ingredients) {
   recipes[dish]: ingredients
   return recipes
 }
-
-1) Objects destructivelyUpdateObjectWithKeyAndValue(object, key, value)
-updates `object` with the given `key` and `value` (it is destructive) and re
-turns the entire updated object:
