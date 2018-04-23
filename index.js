ba41dce8cbd54a8f65f0c73df7ccff26ea0728b7
Michael Eby <michaeleby1@gmail.com>
2018-04-23 00:39:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-var recipes = { 'Pasta': 'Sauce'}
+var recipes = { 'Pasta': 'Sauce'};
 
 function updateObjectWithKeyAndValue(recipes, dish, ingredients) {
-recipes[dish] = ingredients
-return recipes
-}
+  recipes[dish] = ingredients
+  return recipes
+};
+
+function updateObjectWithKeyAndValue(recipes, dish, ingredients) {
+  Object.assign({Pasta: Sauce}, {dish: ingredients})
+};
+
+  1) Objects updateObjectWithKeyAndValue(object, key, value) it does not modify the original object,
+   but rather returns a clone with the new data:
